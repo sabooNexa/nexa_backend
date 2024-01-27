2fa975f34e3edb7892daf2b0a70ac215d21e4c9d
veronica17-web <veronicamiryala001@gmail.com>
2024-01-27 17:05:53 +0530

    added accessories

---
+++ src/model/accessoriesModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const AccessoriesSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      trim: true,
+    },
+
+    model: {
+      //models
+      type: String,
+      trim: true,
+    },
+    product:{
+      type: String,
+      trim: true,
+    },
+    email: {
+      //outlet
+      type: String,
+      trim: true,
+    },
+    leadFrom: {
+      type: String,
+      default: "Accessories",
+    },
+    date: {
+      type: String,
+    },
+    time: {
+      type: String,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Accessories",AccessoriesSchema)
+// email: "testing@gmail.com";
