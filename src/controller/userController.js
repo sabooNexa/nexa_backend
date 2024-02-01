76e39c54a9481aba1463cd2b1ec8f0b07f9b69cf
veronica17-web <veronicamiryala001@gmail.com>
2024-02-01 13:06:27 +0530

    changed expire time

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -45,7 +45,7 @@ const login = async (req, res) => {
       const token = jwt.sign(
         { userID: isEmailExists._id?.toString() },
         process.env.Secret,
-        { expiresIn: '10m' }
+        { expiresIn: '1hr' }
       );
   
       //  Make Respoense
